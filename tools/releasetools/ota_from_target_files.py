d0d35d3165bbf74561e840d18b2c7ec142a067e1
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-01-02 00:16:29 +0100

    OTA: print ROM name when flashing
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -584,6 +584,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  script.Print(" ")
+  script.Print("----------------------------------")
+  script.Print("YAOSP-M - Yank555.lu's AOSP MM ROM")
+  script.Print("----------------------------------")
+  script.Print(" ")
+
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
